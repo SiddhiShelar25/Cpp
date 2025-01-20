1e8517d38b575e9005e38e2a82fcb8c570ee591b
Siddhi Shelar <siddhishelar.sknsits.comp@gmail.com>
2025-01-20 14:41:15 +0530

    Arrays

--- ARRAYS/Vectors/ReverseSpecificPart.cpp
+++ ARRAYS/Vectors/ReverseSpecificPart.cpp
@@ -9,37 +9,37 @@ void display(vector<int> &a)
     }
     cout << endl;
 }
+void reversePart(int i, int j, vector<int>& v){
+    while (i<=j){
+        int temp = v[i];
+        v[i]=v[j];
+        v[j]=temp;
+        i++;
+        j--;
+    }
+    return;
+    
+}
 int main()
 {
     vector<int> v;
     v.push_back(1);
+    v.push_back(6);
     v.push_back(2);
     v.push_back(3);
-    v.push_back(4);
-    v.push_back(5);
-    v.push_back(6);
     v.push_back(7);
-
-   
+    v.push_back(4);
+    
     display(v);
 
-    //reverse
-//     int i=0;
-//     int j=v.size()-1;
-//     while(i<=j){
-       //swap v[i] and v[j]
+    
+// for(int i=0, j=v.size()-1;i<=j;i++,j--){
 //         int temp = v[i];
 //         v[i]=v[j];
 //         v[j]=temp;
-//         i++;
-//         j--;
-//     }
-for(int i=0, j=v.size()-1;i<=j;i++,j--){
-        int temp = v[i];
-        v[i]=v[j];
-        v[j]=temp;
-}
+// }
 //reverse(v.begin(),v.end());
+reversePart(0,2,v);
   display(v);
    
 }
